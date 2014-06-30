b0d003430cd580758ecad2f3f52aa6920fb48b6a
Mark Stevens <stevens.mark.a@gmail.com>
2014-06-30 05:30:58 +0000

    Working on robot rail and drink db

---
+++ drink_db.py
@@ -0,0 +1,9 @@
+import pycurl
+
+class DrinkDatabase(object):
+  def __init__(self):
+    pass
+  def SearchForIngredients(self, drink_name):
+    """Returns a list of ingredients, or an empty list for not found"""
+    #TODO(Mark): alternates
+
--- toggle_pin.py
+++ toggle_pin.py
@@ -24,16 +24,36 @@ def Setup():
   gpio.output(en_pin, 0)
 
 class StepperMotor(object):
-  def __init__(self):
+  def __init__(self, dry_run=False):
     self.pulse_state = False
+    self.dry_run = dry_run
 
   def Move(self, steps, forward=1):
+    if self.dry_run:
+      print "DRY RUN: Moving %d steps in direction: %d" % (steps, forward)
     gpio.output(pul_pin, 0)
     gpio.output(dir_pin, forward)
     for i in range(steps):
       gpio.output(pul_pin, int(self.pulse_state))
       time.sleep(0.001)  # one millisecond
       self.pulse_state = not self.pulse_state
+
+def InchesToSteps(inches):
+  return int(inches / 3.75 * 800)
+
+class RobotRail(object):
+  def __init__(self, motor):
+    self.motor = motor
+    self.position = 0
+
+  def FillPositions(self, absolute_positions):
+    for position in absolute_positions:
+      forward = position > self.position
+      steps = InchesToSteps(abs(position - self.position))
+      self.motor.Move(steps, forward=forward)
+      self.position = position
+      print "At position: %f" % position
+      time.sleep(2.0)
 
   
 def main(args):
@@ -43,30 +63,32 @@ def main(args):
                       help='Steps to move the motor')
   parser.add_argument('--inches', type=float, nargs="?", default=0,
                       help='Inches to move the motor')
-  parser.add_argument('--backward', type=bool, nargs="?", default=False,
+  parser.add_argument('--backward', type=str, nargs="?", default="False",
                       help='Direction to move')
   parser.add_argument('--dry_run', type=bool, nargs="?", default=False,
                       help='Whether to move motors')
+  parser.add_argument('--positions', type=float, nargs="+", default=(),
+                      help='List of positions to move the truck through')
   args = parser.parse_args()
+  print args
   # For the NEMA 14 12v 350mA (#324) stepper motors from AdaFruit:
   # http://www.adafruit.com/products/324
   # Driving it with 12v using a delay of 1 microsecond.
   Setup()
-  #revolutions = 3
-  motor = StepperMotor()
-  #steps = revolutions * 200 * 16
-  forward = not args.backward
+  motor = StepperMotor(args.dry_run)
+  rail = RobotRail(motor)
+  if args.positions:
+    rail.FillPositions(args.positions)
+  forward = (args.backward != "False")
   if args.inches:
-    steps = int(args.inches / 3.75 * 800)
+    steps = InchesToSteps(args.inches)
   else:
     steps = args.steps
   if forward:
     print "Forward %d steps" % steps
   else:
     print "Backward %d steps" % steps
-  if not args.dry_run:
-    print "Running motor"
-    motor.Move(steps, forward=forward)
+  motor.Move(steps, forward=forward)
   gpio.cleanup()
 
 if __name__ == "__main__":
