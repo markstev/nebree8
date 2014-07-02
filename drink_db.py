137084ad0ab14f86c2678eb22f94e48f1e728ad9
Mark Stevens <stevens.mark.a@gmail.com>
2014-07-02 00:28:16 -0700

    Enabled looking up simple drinks from cocktaildb.

--- drink_db.py
+++ drink_db.py
@@ -1,9 +1,111 @@
 import pycurl
+import re
+import sys
+import urllib
+import cStringIO
+ 
 
 class DrinkDatabase(object):
   def __init__(self):
     pass
+
+  def _InitialSearch(self, drink_name):
+    curl = pycurl.Curl()
+    curl.setopt(curl.URL, "www.cocktaildb.com")
+    curl.setopt(curl.FOLLOWLOCATION, 1)
+    curl.setopt(curl.CONNECTTIMEOUT, 5)
+    curl.setopt(curl.TIMEOUT, 8)
+    form_data = {
+        "simpleSearch": drink_name,
+        "_action_submit_name": "Search recipes database",
+        "__formID__": "recipes_name_search",
+        }
+    # url_opts = "&".join(["%s=%s" % (k, urllib.quote(v))
+    #                      for k, v in form_data.iteritems()])
+    url_opts = urllib.urlencode(form_data)
+    curl.setopt(curl.POSTFIELDS, url_opts)
+    curl.setopt(curl.POST, 1)
+    curl.setopt(curl.COOKIEJAR, "cookie.txt")
+    buf = cStringIO.StringIO()
+    curl.setopt(curl.WRITEFUNCTION, buf.write)
+    try:
+      curl.perform()
+    except pycurl.error, error:
+      print error
+    return buf.getvalue()
+
+  def _ParseFirstPage(self, first_page_html):
+    #if 
+    recipes = re.findall("recipe_detail\?id=\d+\" title=\"click here for the recipe\">[\w ]+</a>",
+                         first_page_html)
+    recipe_name_and_url = []
+    for recipe in recipes:
+      m = re.match("(recipe_detail\?id=\d+)\" .+recipe\">([\w ]+)</a>", recipe)
+      recipe_name_and_url.append((m.group(2), m.group(1)))
+    return recipe_name_and_url
+
+  def _GetSecondPage(self, url_suffix):
+    curl = pycurl.Curl()
+    curl.setopt(curl.URL, "www.cocktaildb.com/%s" % url_suffix)
+    curl.setopt(curl.FOLLOWLOCATION, 1)
+    curl.setopt(curl.CONNECTTIMEOUT, 5)
+    curl.setopt(curl.TIMEOUT, 8)
+    buf = cStringIO.StringIO()
+    curl.setopt(curl.WRITEFUNCTION, buf.write)
+    try:
+      curl.perform()
+    except pycurl.error, error:
+      print error
+    return buf.getvalue()
+
+  def _ParseSecondPage(self, second_page_html):
+    ingredients_html = re.findall("\"recipeMeasure\">.*<a href=\"ingr_detail\?id=.*\">.*</a>",
+                                  second_page_html)
+    ingredients = []
+    for ingredient in ingredients_html:
+      m = re.match("\"recipeMeasure\">(.*)<a href=\"ingr_detail\?id=.*\">(.*)</a>", ingredient)
+      ingredients.append((m.group(2), self.ParseVolume(m.group(1))))
+    return ingredients
+
   def SearchForIngredients(self, drink_name):
     """Returns a list of ingredients, or an empty list for not found"""
     #TODO(Mark): alternates
+    recipe_urls = self._ParseFirstPage(self._InitialSearch(drink_name))
+    for name, url in recipe_urls:
+      if name.lower() == drink_name.lower():
+        page = self._GetSecondPage(url)
+        return self._ParseSecondPage(page)
 
+  def ParseVolume(self, volume):
+    volume = volume.strip().rstrip()
+    if " oz" in volume:
+      units = "oz"
+      volume = volume.replace(" oz", "")
+    else:
+      return (None, None)
+    total = 0.0
+    for portion in volume.split(" "):
+      try:
+        if "/" in portion:
+          num, skip, denom = portion.partition("/")
+          total += float(num) / float(denom)
+        else:
+          total += float(portion)
+      except ValueError, e:
+        print portion
+        raise e
+    return (units, total)
+
+
+def main(args):
+  drink_db = DrinkDatabase()
+  print drink_db.ParseVolume("1 oz")
+  print drink_db.ParseVolume("1/4 oz")
+  print drink_db.ParseVolume("2 1/2 oz")
+  ingredients = drink_db.SearchForIngredients("martini")
+  for ingredient in ingredients:
+    print "Ingredient: %s; Volume: %f %s" % (ingredient[0], ingredient[1][1], ingredient[1][0])
+
+
+if __name__ == "__main__":
+  main(sys.argv)
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+#sudo apt-get install libcurl
+sudo apt-get install python-pip
+sudo pip install --upgrade pip
+
+sudo apt-get install libxml2
+
+pip install pycurl
+sudo pip install pyquery
+
