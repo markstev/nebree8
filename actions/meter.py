29a9cf36e595708e11ede9f10fc50b67ad5b5a1f
Sagar Mittal <sagar@sagarmittal.com>
2014-09-13 23:45:32 +0000

    Recalibrate

--- actions/meter.py
+++ actions/meter.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 from time import time, sleep
 
 VALVE_ACTUATION_DELAY_SECS = 0.3
-OZ_TO_ADC_VALUES = 42.
+OZ_TO_ADC_VALUES = 38.35
 MAX_TARE_STDDEV = 2.
 TARE_TIMEOUT_SECS = 2.
 MAX_METER_SECS = 15.
