7d2bb352717ed46e3d386b5f1d0ba1da7ec8d75a
Sagar Mittal <sagar@sagarmittal.com>
2014-09-14 05:52:53 +0000

    Fixes for SimpleMeter

--- actions/meter_simple.py
+++ actions/meter_simple.py
@@ -12,16 +12,13 @@ class MeterSimple(Action):
     if self.oz_to_meter == 0:
       logging.warning("oz_to_meter was zero, returning early.")
     tare = _tare(robot)
-    self.target_reading = (tare.mean + OZ_TO_ADC_VALUES * self.oz_to_meter -
-                           OZ_TO_ADC_VALUES * .1)
-    
-    t = self.oz_to_meter * SECONDS_PER_OZ
+    self.target_reading = (tare.mean + OZ_TO_ADC_VALUES * self.oz_to_meter)
     with robot.OpenValve(self.valve_to_actuate):
-      while robot.load_cell.recent_summary(secs=.1).mean < target_reading:
+      while robot.load_cell.recent_summary(secs=.1).mean < self.target_reading:
         time.sleep(.05)
     time.sleep(1)
     r = robot.load_cell.recent(secs = time.time() - tare.timestamp + 5)
-    f = open('readings_%s_%fs.csv' % (
-        time.strftime("%Y%m%d_%H%M%S"), t), 'w')
+    f = open('readings_%s_%foz.csv' % (
+        time.strftime("%Y%m%d_%H%M%S"), self.oz_to_meter), 'w')
     for ts, v in r: print >>f, "%s,%s" % (ts, v)
     f.close()
